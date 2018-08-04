ff678e9c5916e13d42f21d98236acc63d4b3637c
tunisiano187 <fabian@bowlman.org>
2018-08-04 09:28:05 +0000

    Adding the reset link in the upper menu than come back to the first page

--- WME-Filter.user.js
+++ WME-Filter.user.js
@@ -3,7 +3,7 @@
 // @namespace      https://www.bowlman.org
 // @description    Filter the PURs results on the wmebr website to only show what we can handle
 // @match          https://wmebr.info/ur/purs_on_state.php*
-// @version        2018.08.04.01
+// @version        2018.08.04.02
 // @author         tunisiano187 '2018
 // @license        MIT/BSD/X11
 // @compatible     chrome firefox
@@ -15,12 +15,23 @@
 (function() {
     'use strict';
 
+    var fullpath = "https://wmebr.info/ur/purs_on_state.php";
+
     if(window.location.hash == ("#reset-WMEBR-editor-level")) {
         localStorage.removeItem('WME-editor-level');
         alert("Level resetted");
-        var fullpath = window.location.pathname+window.location.search;
+        if('WMEBR-last-page' in localStorage) {
+            fullpath = localStorage.getItem('WMEBR-last-page');
+        }
         window.location.href = fullpath;
     }
+    // Adding the link to the menu
+    $("#navbar ul.navbar-nav").append('<li><a href="purs_on_state.php#reset-WMEBR-editor-level">Reset editor level</a></li>');
+
+    // Getting the last query
+    fullpath = window.location.pathname+window.location.search;
+    localStorage.setItem('WMEBR-last-page',fullpath);
+
     var WMEeditorlevel=2;
 
     if('WME-editor-level' in localStorage && localStorage.getItem('WME-editor-level') !=='0') {
